0403e365cc0bbfcb753ba5e83b13ccaedcb76dda
aimanihsan421 <130918500+aimanihsan421@users.noreply.github.com>
2023-04-17 04:12:16 +0800

    Add files via upload

---
+++ app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'video',
+    loadChildren: () => import('./video/video.module').then( m => m.VideoPageModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
+  },
+  {
+    path: 'promotion',
+    loadChildren: () => import('./promotion/promotion.module').then( m => m.PromotionPageModule)
+  },
+  {
+    path: 'membership',
+    loadChildren: () => import('./membership/membership.module').then( m => m.MembershipPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDDmcvA2uNOWmeMS9_8PENOi4gL0kZnl2k",
+    authDomain:"project-9d04d.firebaseapp.com",
+    projectId: "project-9d04d.appspot.com",
+    storageBucket:"project-9d04d.appspot.com",
+    messagingSenderId: "215823387829",
+    appId: "1:215823387829:web:92c3ba01b606a9858942fc",
+    measurementId: "G-KENXP4BXWE"
+  }
+  
+};
